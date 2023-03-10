94c6103eb20fd4ecf914eaded5eb168549891b54
Fabio Fognani <fabio.fognani@gmail.com>
2023-03-10 21:53:03 +0100

    feat: setup alias for absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 import svgrPlugin from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
@@ -7,6 +8,9 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  resolve: {
+    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }]
+  },
   test: {
     globals: true,
     environment: 'jsdom',
